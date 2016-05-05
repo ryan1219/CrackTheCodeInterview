fb5dad3b61de94c384f9bbf4a2cea3f9863a861c
Yan Ren <ryan.ren3@gmail.com>
2016-05-05 16:52:47 -0400

    Initial Checkin

---
+++ arraysAndStrings/src/arraysAndStrings/Q1_1.java
@@ -0,0 +1,37 @@
+package arraysAndStrings;
+
+public class Q1_1 {
+	//where is the rep
+	//sort the array in ASCII order
+	public static boolean isUniqueChar(String str){
+		//sort
+		
+		//
+		for(int i =0; i <str.length()-1; i++){
+			if(str.charAt(i)==str.charAt(i+1)) return false;
+		}
+		return true;
+	}
+	//Build an ASCII table
+	public static boolean isUniqueChars2(String str) {
+		if (str.length() > 128) {
+			return false;
+		}
+		boolean[] char_set = new boolean[128];
+		for (int i = 0; i < str.length(); i++) {
+			int val = str.charAt(i);
+			if (char_set[val]) return false;
+			char_set[val] = true;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		String[] words = {"abcde", "helblo", "apple", "kite", "padle"};
+		for (String word : words) {
+			System.out.println(word + ": " + isUniqueChars2(word));
+		}
+		//System.out.println((1<<-3));
+	}
+
+}
