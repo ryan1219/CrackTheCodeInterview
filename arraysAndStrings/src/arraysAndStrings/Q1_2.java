a4e87821f7e854cc86dbb7a098980e32bcb6804d
Yan Ren <ryan.ren3@gmail.com>
2016-05-06 08:41:59 -0400

    Q1_2 finished

---
+++ arraysAndStrings/src/arraysAndStrings/Q1_2.java
@@ -0,0 +1,23 @@
+package arraysAndStrings;
+/*
+ * reverse a string in java
+ */
+public class Q1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "abcdefg1234567890";
+		System.out.println("original string "+ a);
+		System.out.println("reverse string "+ reverse(a));
+	}
+
+	public static String reverse(String str){
+		char[] temp = new char[str.length()];
+		int index = 0;
+		for(int i = str.length()-1; i>=0; i--){
+			temp[index]=str.charAt(i);
+			index++;
+		}
+		return new String(temp);
+	}
+}
---
+++ linkedList/src/linkedList/Q2_1.java
@@ -0,0 +1,10 @@
+package linkedList;
+
+public class Q2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
