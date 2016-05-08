cd2a22f8befaf40ad9047731d9a9d7b61cacfd66
Yan Ren <ryan.ren3@gmail.com>
2016-05-08 13:35:39 -0400

    Q1_3 starts

---
+++ arraysAndStrings/src/arraysAndStrings/Q1_3.java
@@ -0,0 +1,18 @@
+package arraysAndStrings;
+
+/*
+ * print out the list of permutation strings of an input string x 
+ */
+public class Q1_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+	
+	public static void printAllPermutation(String str){
+		
+	}
+
+}
