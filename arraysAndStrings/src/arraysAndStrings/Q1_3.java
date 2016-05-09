220087339b2f073034d46cb78fa8be9fdaab7e63
Yan Ren <ryan.ren3@gmail.com>
2016-05-09 17:03:18 -0400

    Q1_3 need to fix the bug

--- arraysAndStrings/src/arraysAndStrings/Q1_3.java
+++ arraysAndStrings/src/arraysAndStrings/Q1_3.java
@@ -11,8 +11,32 @@ public class Q1_3 {
 
 	}
 	
-	public static void printAllPermutation(String str){
-		
+	public static void printAllPermutation(String input){
+		int inputLength = input.length();
+		boolean[ ] used = new boolean[ inputLength ];
+		StringBuffer outputString = new StringBuffer();
+		char[ ] in = input.toCharArray( );
+		  
+		doPermute ( in, outputString, used, inputLength, 0 );
+	}
+	public static void doPermute( char[ ] in, StringBuffer outputString, 
+            boolean[ ] used, int inputLength, int level){
+		if( level == inputLength) {
+			System.out.println ( outputString.toString()); 
+			return;
+		}
+
+		for( int i = 0; i < inputLength; ++i )
+		{       
+
+			if( used[i] ) continue;
+
+			outputString.append( in[i] );      
+			used[i] = true;       
+			doPermute( in,   outputString, used, length, level + 1 );       
+			used[i] = false;       
+			outputString.setLength(   outputString.length() - 1 );   
+		}
 	}
 
 }
