b4fd09cf273752654f29e69fda8d80e5a8c83c4d
Yan Ren <ryan.ren3@gmail.com>
2016-05-27 00:12:50 -0400

    Q2_2 not complete

---
+++ linkedList/src/linkedList/Q2_2.java
@@ -0,0 +1,19 @@
+package linkedList;
+
+/*
+ * 2.2 Implement an algorithm to find the kth to last element of a singly linked list.
+
+ */
+public class Q2_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static linkedList kthToLast(linkedList a, int k){
+		linkedList result = new linkedList();
+		//for(,k<linkedList.size)
+		return null; 
+	}
+}
