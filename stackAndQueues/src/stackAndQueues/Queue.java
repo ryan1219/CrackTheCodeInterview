a1246d0d42516edc50d597bfb518f8611317905b
Yan Ren <ryan.ren3@gmail.com>
2017-09-07 00:05:17 -0400

    simple queue

--- stackAndQueues/src/stackAndQueues/Queue.java
+++ stackAndQueues/src/stackAndQueues/Queue.java
@@ -1,5 +1,24 @@
 package stackAndQueues;
 
 public class Queue {
+	Node head, tail;
 
+	void enqueue(Object item) {
+		if (head == null) {
+			tail = new Node(item);
+			head = tail;
+		} else {
+			head.next = new Node(item);
+			head = head.next;
+		}
+	}
+
+	Object dequeue() {
+		if (tail != null) {
+			Object item = tail.data;
+			tail = tail.next;
+			return item;
+		}
+		return null;
+	}
 }
