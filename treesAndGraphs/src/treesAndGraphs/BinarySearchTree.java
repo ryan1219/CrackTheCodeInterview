6e33ec51b90d682a5951ae4bebc793dc5e540524
Yan Ren <ryan.ren3@gmail.com>
2018-05-16 21:51:38 -0400

    BST starts

---
+++ treesAndGraphs/src/treesAndGraphs/BinarySearchTree.java
@@ -0,0 +1,30 @@
+package treesAndGraphs;
+
+public class BinarySearchTree {
+	public static void main(String[] args){
+		
+	}
+	
+	Node root;
+	
+	private Node addRecursive(Node current, int value) {
+	    if (current == null) {
+	        return new Node(value);
+	    }
+	 
+	    if (value < current.value) {
+	        current.left = addRecursive(current.left, value);
+	    } else if (value > current.value) {
+	        current.right = addRecursive(current.right, value);
+	    } else {
+	        // value already exists
+	        return current;
+	    }
+	 
+	    return current;
+	}
+	
+	public void add(int value){
+		addRecursive(root, value);
+	}
+}
