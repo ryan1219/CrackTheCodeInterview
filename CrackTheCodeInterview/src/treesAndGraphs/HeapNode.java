7220f7ea7902ac108ecb990c3f3df277862814f0
Yan Ren <ryan.ren3@gmail.com>
2020-04-22 12:52:36 -0400

    add linked tree heap implementation

---
+++ CrackTheCodeInterview/src/treesAndGraphs/HeapNode.java
@@ -0,0 +1,16 @@
+package treesAndGraphs;
+
+public class HeapNode {
+	int value;
+	HeapNode left;
+	HeapNode right;
+	HeapNode parent;
+	
+	HeapNode(int value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+		this.parent = null;
+	}
+
+}
---
+++ CrackTheCodeInterview/src/treesAndGraphs/NodeHeap.java
@@ -0,0 +1,103 @@
+package treesAndGraphs;
+
+public class NodeHeap implements Heap {
+
+	private HeapNode root;
+	private int size;
+
+	private HeapNode getLastNode() {
+		String binary = Integer.toBinaryString(size);
+		HeapNode current = root;
+		for (int i = 1; i < binary.length(); i++) {
+			if (binary.charAt(i) == '0') {
+				current = current.left;
+			} else {
+				current = current.right;
+			}
+		}
+		return current;
+	}
+
+	private void insertLast(int value) {
+		String binary = Integer.toBinaryString(size + 1);
+		HeapNode current = root;
+		for (int i = 1; i < binary.length() - 1; i++) {
+			if (binary.charAt(i) == '0') {
+				current = current.left;
+			} else {
+				current = current.right;
+			}
+		}
+
+		if (binary.charAt(binary.length() - 1) == '0') {
+			current.left = new HeapNode(value);
+		} else {
+			current.right = new HeapNode(value);
+		}
+
+		size++;
+	}
+
+	private void removeLast() {
+		HeapNode last = getLastNode();
+		if(last.parent.left == last) {
+			last.parent.left = null;
+		}else {
+			last.parent.right = null;
+		}
+		last.parent = null;
+	}
+
+	/*
+	 * swap the value inside two nodes, i and j
+	 */
+	private void swap(HeapNode i, HeapNode j) {
+		int temp = i.value;
+		i.value = j.value;
+		j.value = temp;
+	}
+
+	private void heapifyUp(HeapNode j) {
+		while (j != root) {
+			HeapNode p = j.parent;
+			if (j.value > p.value) {
+				break;
+			}
+			swap(j, p);
+			j = p;
+		}
+	}
+
+	private void heapifyDown(HeapNode j) {
+		while (j.left != null) {
+			HeapNode min = j.left;
+			if (j.right != null) {
+				if (j.right.value < j.left.value) {
+					min = j.right;
+				}
+			}
+
+			if (j.value < min.value) {
+				break;
+			}
+			swap(j, min);
+			j = min;
+		}
+	}
+
+	@Override
+	public void insert(int i) {
+		insertLast(i);
+		heapifyUp(getLastNode());
+	}
+
+	@Override
+	public int remove() throws Exception {
+		int result = root.value;
+		swap(root, getLastNode());
+		removeLast();
+		heapifyDown(root);
+		return result;
+	}
+
+}
