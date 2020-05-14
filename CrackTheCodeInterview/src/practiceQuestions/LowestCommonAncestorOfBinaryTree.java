491555b9a6e0f3fbe5b7bdecdd881c962e23d3a2
Yan Ren <ryan.ren3@gmail.com>
2020-05-13 22:27:51 -0400

    add questions

---
+++ CrackTheCodeInterview/src/practiceQuestions/LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,37 @@
+package practiceQuestions;
+
+/*
+ * question: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ */
+public class LowestCommonAncestorOfBinaryTree {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (root == null) {
+			return null;
+		}
+		if (root.val == p.val || root.val == q.val) {
+			return root;
+		}
+		TreeNode left = lowestCommonAncestor(root.left, p, q);
+		TreeNode right = lowestCommonAncestor(root.right, p, q);
+		if (left != null && right != null) {
+			return root;
+		}
+		if (left != null) {
+			return left;
+		}
+		if (right != null) {
+			return right;
+		}
+		return null;
+	}
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
