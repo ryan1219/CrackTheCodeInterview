eff2e3488af866172c01401ed68eb92fa9074c01
Yan Ren <ryan.ren3@gmail.com>
2020-07-09 21:19:33 -0400

    add question

---
+++ CrackTheCodeInterview/src/practiceQuestions/PermutationSequence.java
@@ -0,0 +1,33 @@
+package practiceQuestions;
+
+import java.util.LinkedList;
+
+public class PermutationSequence {
+	public String getPermutation(int n, int k) {
+		String res = "";
+		LinkedList<Integer> order = new LinkedList<>();
+		for (int i = 1; i <= n; i++) {
+			order.add(i);
+		}
+		k -= 1;
+		while(n > 0) {
+			int divisor = factorial(n - 1);
+			int quotient = k / divisor;
+			k = k % divisor;
+			n -= 1;
+			res += order.get(quotient);
+			order.remove(quotient);
+		}
+
+		return res;
+	}
+	
+	public int factorial(int n) {
+		int res = 1;
+		while(n > 0) {
+			res = res  * n;
+			n -= 1;
+		}
+		return res;
+	}
+}
