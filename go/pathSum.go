66a6595ec66441851da82ff68a3fc6ea4c57f41f
Yan Ren <ryan.ren3@gmail.com>
2021-01-11 21:00:40 -0500

    add questions

---
+++ go/pathSum.go
@@ -0,0 +1,24 @@
+package main
+
+func hasPathSum(root *TreeNode, sum int) bool {
+	result := false
+	find := &result
+
+	inorder(root, 0, sum, find)
+	return *find
+}
+
+func inorder(root *TreeNode, sum, target int, find *bool) {
+	if root == nil {
+		return
+	}
+
+	if root.Left == nil && root.Right == nil {
+		if sum+root.Val == target {
+			*find = true
+		}
+	}
+
+	inorder(root.Left, sum+root.Val, target, find)
+	inorder(root.Right, sum+root.Val, target, find)
+}
---
+++ go/symmetricTree.go
@@ -0,0 +1,39 @@
+package main
+
+import "strconv"
+
+func isSymmetric(root *TreeNode) bool {
+	pre := ""
+	post := ""
+
+	preorder(root, &pre)
+	postorder(root, &post)
+
+	if pre == post {
+		return true
+	}
+
+	return false
+}
+
+func preorder(root *TreeNode, result *string) {
+	if root == nil {
+		(*result) = (*result) + "null" + ","
+		return
+	}
+
+	(*result) = (*result) + strconv.Itoa(root.Val) + ","
+	preorder(root.Left, result)
+	preorder(root.Right, result)
+}
+
+func postorder(root *TreeNode, result *string) {
+	if root == nil {
+		(*result) = (*result) + "null" + ","
+		return
+	}
+
+	(*result) = (*result) + strconv.Itoa(root.Val) + ","
+	postorder(root.Right, result)
+	postorder(root.Left, result)
+}
