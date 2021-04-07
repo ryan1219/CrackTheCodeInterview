8796ff47de55ae2887d31eff1cdca7ccea3fd6df
Yan Ren <ryan.ren3@gmail.com>
2021-04-06 22:40:28 -0400

    height checker

---
+++ go/height_checker.go
@@ -0,0 +1,27 @@
+package main
+
+// question: https://leetcode.com/problems/height-checker/
+func heightChecker(heights []int) int {
+	count := make([]int, 101)
+
+	for _, height := range heights {
+		count[height]++
+	}
+
+	unordered := 0
+	head := 0
+
+	for _, height := range heights {
+		for count[head] == 0 {
+			head++
+		}
+
+		if head != height {
+			unordered++
+		}
+
+		count[head]--
+	}
+
+	return unordered
+}
